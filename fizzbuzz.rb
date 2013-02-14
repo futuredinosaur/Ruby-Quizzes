e2e3ef205975fdc41ea201e218cc8841ca044f4e
David Lee <davedotlee@gmail.com>
2013-02-14 12:29:05 -0800

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,13 @@
+=BEGIN
+FizzBuzz Challenge
+
+Output a list from 1 to 100, one per line, with the following exceptions: 
+Numbers divisible by 3 should display "Fizz" instead of the number. 
+Numbers divisible by 5 should display "Buzz" instead of the number. 
+Numbers divisible by both 3 and 5 should display "FizzBuzz" instead of the number.
+
+=END
+
 (1..100).each do |x|
 fizz = x % 3 == 0
 buzz = x % 5 == 0
